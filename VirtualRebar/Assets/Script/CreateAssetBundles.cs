f74b912063915da8e62e9eab4bec6b31c28909dc
Jerry <jerrylewisbh@gmail.com>
2020-05-19 15:35:34 -0300

    asse bundles

--- VirtualRebar/Assets/Script/CreateAssetBundles.cs
+++ VirtualRebar/Assets/Script/CreateAssetBundles.cs
@@ -5,6 +5,6 @@ public class CreateAssetBundles
     [MenuItem ("Assets/Build AssetBundles")]
     static void BuildAllAssetBundles ()
     {
-        BuildPipeline.BuildAssetBundles ("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneOSXUniversal);
+        BuildPipeline.BuildAssetBundles ("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneOSX);
     }
 }
